2c2fe3911acdb3a02b31366684fa6fb5227ca434
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2023-09-08 15:28:27 +0200

    Create algorythme_rev.py
    add somme function

---
+++ algorythme_rev.py
@@ -0,0 +1,14 @@
+## importation des modules
+
+
+## Déclaration des fonction
+
+def somme(n):
+    ans = 0
+    for i in range(n):
+        ans += i
+    return ans
+
+## programme principale
+resultat = somme(5)
+print(resultat)
