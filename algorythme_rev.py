56402700ee8a8e715763542fcf48f160ce9c2b87
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2023-09-08 15:41:41 +0200

    Update algorythme_rev.py
    ADD somme_tableau func

--- algorythme_rev.py
+++ algorythme_rev.py
@@ -5,9 +5,13 @@
 
 def somme(n):
     ans = 0
-    for i in range(n):
+    for i in range(n+1):
         ans += i
     return ans
+
+def somme_tableau(table):
+    ...
+    
 
 ## programme principale
 resultat = somme(5)
