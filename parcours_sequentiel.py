6d52ea946a1faa12bd9146a7dfe655eb39581ff6
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2023-09-11 12:18:02 +0200

    Create parcours_sequentiel.py

---
+++ parcours_sequentiel.py
@@ -0,0 +1,22 @@
+## importation des modules
+
+
+## Déclaration des fonction
+
+
+def somme_tableau(t):
+    ans = 0
+    for i in t:
+        ans += int(i)
+    return ans
+
+def average_table(t):
+    ans = somme_tableau(t)
+    ans = ans /len(t)
+    return ans
+def main():
+    
+    t = [5,8,12,1,6,14,13,22,3,87]
+    average_table(t)
+
+if
