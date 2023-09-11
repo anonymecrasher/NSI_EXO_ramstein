c7fcab537485cbf7ac7071de81179bdf205b47ca
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2023-09-11 12:25:56 +0200

    Update parcours_sequentiel.py
    Q4

--- parcours_sequentiel.py
+++ parcours_sequentiel.py
@@ -11,13 +11,24 @@ def somme_tableau(t):
     return ans
 
 def average_table(t):
+    """
+    doc ...
+    """
     ans = somme_tableau(t)
     ans = ans /len(t)
     return ans
 def main():
     
+    help(average_table)
+    
     t = [5,8,12,1,6,14,13,22,3,87]
     print(average_table(t))
+
+
+
+
+## Programme principal
+
 
 if __name__ == '__main__':
     main()
