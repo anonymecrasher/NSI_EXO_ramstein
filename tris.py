bf8e0e412cafa95a02e0114ac780942dec21eac1
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2023-09-15 15:39:28 +0200

    Create tris.py

---
+++ tris.py
@@ -0,0 +1,33 @@
+## importation des modules
+
+## Déclaration des fonction
+def plus_petit(tableau):
+    mini = 0
+    for i in range(len(tableau)):
+        if tableau[i] < tableau[mini]:
+            mini = i
+    return mini
+
+
+def tri_selection(tableau):
+    tpt = tableau
+    tt = []
+    for i in range(len(tableau)):
+        pp = plus_petit(tpt)
+        tt.append(tpt[pp])
+        tpt = tpt[:pp] + tpt[pp+1:]
+    return tt
+        
+        
+        
+        
+def main():
+    print(tri_selection([16, 234, 129, 195, 168, 101, 143, 214, 25, 90, 56, 133, 178, 217, 219, 96, 81, 163, 73, 220, 107, 12, 253, 71, 126, 237, 255, 224, 23, 27, 30, 215, 51, 20, 164]))
+    print(plus_petit([16, 234, 129, 195, 168, 101, 143, 214, 25, 90, 56, 133, 178, 217, 219, 96, 81, 163, 73, 220, 107, 12, 253, 71, 126, 237, 255, 224, 23, 27, 30, 215, 51, 20, 164]))
+    
+    
+## Programme principal
+
+
+if __name__ == '__main__':
+    main()
