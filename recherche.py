6f5d1c56d0b20c61e4599c1d3e0b61815d550196
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2023-09-18 12:02:50 +0200

    Update recherche.py

--- recherche.py
+++ recherche.py
@@ -1,10 +1,14 @@
+## importation des modules
+
+## Déclaration des fonction
 def rech_dich(liste, el):
+    liste.sort()
     n = len(liste)
     deb = 0
     fin = n - 1
-    pos = None
+    pos = "rien"
     trouve = False
-    while trouve == False and deb <= fin:
+    while trouve == False and deb < fin:
         mid = (deb + fin) // 2
         if liste[mid] < el:
             fin = mid + 1
@@ -13,6 +17,19 @@ def rech_dich(liste, el):
         elif liste[mid] == el:
             trouve = True
             pos = mid
+        print('p')
     return pos
-            
+
+
+
+
+
+
+def main():
+    t = [9,8,6,7,6,3,1,2,6,45,2,4]
+    print(rech_dich(t, 5))
+
+## Programme principal
+if __name__ == '__main__':
+    main()
     
