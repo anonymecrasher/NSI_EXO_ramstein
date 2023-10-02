f03121a74e5df7d29957931152ac37ca73c86cfd
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2023-10-02 12:24:05 +0200

    Update bouquet.py

--- bouquet.py
+++ bouquet.py
@@ -3,7 +3,7 @@
 ## declaration des class
 
 class Fleur:
-    def __init__(self, couleur='blanc', taille=20, nb_petales=10, nb_feuilles=0):
+    def __init__(self, couleur=['blanc'], taille=20, nb_petales=10, nb_feuilles=0):
         self.couleur = couleur
         self.taille = taille
         self.nb_petales = nb_petales
@@ -35,9 +35,23 @@ class Fleur:
             return False
         else:
             return True
-    
+    def croisement(self, fleur_one):
+        child_fleur = Fleur(fleur_one.couleur + self.couleur,
+                            round((fleur_one.taille + self.taille)/2),
+                            round((fleur_one.nb_petales + self.nb_petales)/2),
+                            round((fleur_one.nb_feuilles + self.nb_feuilles)/2))
+        return child_fleur
         
-
+class Laboratoire_genetique:
+    def __init__(self):
+        pass
+    def croisement(self, fleur_one, f2):
+        child_fleur = Fleur(fleur_one.couleur + f2.couleur,
+                            round((fleur_one.taille + f2.taille)/2),
+                            round((fleur_one.nb_petales + f2.nb_petales)/2),
+                            round((fleur_one.nb_feuilles + f2.nb_feuilles)/2))
+        return child_fleur
+        
 
 ## declaration des fonction
         
@@ -50,10 +64,15 @@ def main():
     fleur_2 = Fleur(taille=30)
     fleur_3 = Fleur()
     
-    fleur_2.couleur = 'rouge'
+    fleur_2.couleur = ['rouge']
     fleur_2.nbr_petales = 8
     bouquet = [fleur_1, fleur_2, fleur_3]
     fleur_1.allonge(999)
+    
+    fleur_4 = fleur_1.croisement(fleur_2)
+    labo = Laboratoire_genetique
+    
+    
     
 
 ## programme principal
