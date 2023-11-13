ffd5d6669928a1d1eb7745a5f7df8b6e6a0aee82
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2023-11-13 11:56:20 +0100

    Update interface_liste_chaine.py

--- interface_liste_chaine.py
+++ interface_liste_chaine.py
@@ -15,13 +15,72 @@ class Liste_chainee():
         
     def est_vide(self):
         return self.tete == None
+    
     def ajouter_element_queue(self, valeur=0):
+        if self.est_vide():
+            self.tete = Maillon(valeur)
+        else:
+            maillon = self.tete
+            while maillon.suivant != None:
+                maillon = maillon.suivant
+            maillon.suivant = Maillon(valeur)
+    def list(self):
+        if self.est_vide():
+            return []
+        else:
+        
+            temp= []
+            maillon = self.tete
+            while maillon.suivant != None:
+                temp.append(maillon.valeur)
+                maillon = maillon.suivant
+            temp.append(maillon.valeur)
+                
+            return temp
+    
+    def len(self):
+        if self.est_vide():
+            return 0
+        else:
+            temp = 1
+            maillon = self.tete
+            while maillon.suivant != None:
+                maillon = maillon.suivant
+                temp += 1
+            return temp
+    
+    def extract_element(self,pos=0):
+        assert pos < self.len(), 'index out of range'
+        assert pos >= 0, 'index out of range'
+        if self.est_vide():
+            return 0
+        else:
+            maillon = self.tete
+            for i in range(pos):
+                maillon = maillon.suivant
+            return maillon.valeur
+    
+    def ajouter_element_tete(self,valeur = 0):
+        self.tete = Maillon(valeur, self.tete)
+            
+        
         
         
 
 # fonction principal
 def main():
     l1 = Liste_chainee()
+    
+    print(l1.tete)
+    l1.ajouter_element_queue(5)
+    print(l1.tete.valeur)
+    l1.ajouter_element_queue(7)
+    print(l1.list())
+    print(l1.len())
+    print(l1.extract_element(1))
+    l1.ajouter_element_tete()
+    print(l1.list())
+    
 
 # programme principal
 if __name__ == '__main__':
