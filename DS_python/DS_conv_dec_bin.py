c67abc0807c48be53a1e5fd8f50a503473a7d9ea
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2024-01-16 11:50:06 +0100

    Update DS_conv_dec_bin.py

--- DS_python/DS_conv_dec_bin.py
+++ DS_python/DS_conv_dec_bin.py
@@ -72,30 +72,67 @@ def rom_to_dec(nb_rom:str)->int:
 
 
 def dec_to_rom(nb_dec:int)->str:
-    if nb_dec >= 1000:
-        add = 'M'
-    elif nb_dec >= 900 and nb_dec < 1000:
-        add = 'CM'
-    elif nb_dec >= 990 and nb_dec < 1000:
-        add = 'XM'
-     elif nb_dec >= 999 and nb_dec < 1000:
-        add = 'IM'
-    elif nb_dec >= 500:
-        add = 'D'
-    elif nb_dec >= 400 and nb_dec < 500:
-        add = 'CD'
-    elif nb_dec >= 490 and nb_dec < 500:
-        add = 'XD'
-    elif nb_dec >= 499 and nb_dec < 500:
-        add = 'ID'
-    elif nb_dec >= 100:
-        add = 'C'
-    elif nb_dec >= 90 and nb_dec < 100:
-        add = 'XC'
-    elif nb_dec >= 99 and nb_dec < 100:
-        add = 'IC'
-    elif nb_dec >= 50:
-        pass
+    nb_rom = ''
+    while not nb_dec == 0:
+        if nb_dec >= 1000:
+            add = 'M'
+            nb_dec -= 1000
+        elif nb_dec >= 900 and nb_dec < 1000:
+            add = 'CM'
+            nb_dec -= 900
+        elif nb_dec >= 990 and nb_dec < 1000:
+            add = 'XM'
+            nb_dec -= 990
+        elif nb_dec >= 999 and nb_dec < 1000:
+            add = 'IM'
+            nb_dec -= 999
+        elif nb_dec >= 500:
+            add = 'D'
+            nb_dec -= 500
+        elif nb_dec >= 400 and nb_dec < 500:
+            add = 'CD'
+            nb_dec -= 400
+        elif nb_dec >= 490 and nb_dec < 500:
+            add = 'XD'
+            nb_dec -= 490
+        elif nb_dec >= 499 and nb_dec < 500:
+            add = 'ID'
+            nb_dec -= 499
+        elif nb_dec >= 100:
+            add = 'C'
+            nb_dec -= 100
+        elif nb_dec >= 90 and nb_dec < 100:
+            add = 'XC'
+            nb_dec -= 90
+        elif nb_dec >= 99 and nb_dec < 100:
+            add = 'IC'
+            nb_dec -= 99
+        elif nb_dec >= 50:
+            add = 'L'
+            nb_dec -= 50
+        elif nb_dec >= 40 and nb_dec < 50:
+            add = 'XL'
+            nb_dec -= 40
+        elif nb_dec >= 49 and nb_dec < 50:
+            add = 'IL'
+            nb_dec -= 49
+        elif nb_dec >= 10:
+            add = 'X'
+            nb_dec -= 10
+        elif nb_dec >= 9 and nb_dec <= 10:
+            add = 'IX'
+            nb_dec -= 9
+        elif nb_dec >= 5:
+            add = 'V'
+            nb_dec -= 5
+        elif nb_dec == 4:
+            add = 'IV'
+            nb_dec -= 4
+        elif nb_dec >= 1:
+            add = 'I'
+            nb_dec -= 1
+        nb_rom += add
+    return nb_rom
     
 
 
@@ -106,6 +143,7 @@ def main():
     print(dec_to_bin(28))
     print(bin_to_dec('101011'))
     print(rom_to_dec('MMXXIV'))
+    print(dec_to_rom(2024))
 
 ## programme principal
 if __name__ == '__main__':
