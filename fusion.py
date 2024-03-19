4d0c4a36475b66e2f07726838c8d183fc23d54bb
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2024-03-19 11:15:32 +0100

    Update fusion.py

--- fusion.py
+++ fusion.py
@@ -1,29 +1,53 @@
 ## Importation des modules
+import random
+
 
 ## declaration des consante
 
 ## Déclaration des fonctions
 def fusion_listes(liste_1, liste_2):#liste trie
     liste_r = []
-    list_1 = liste_1.copy()
-    list_2 = liste_2.copy()
-    while len(list_1) != 0 or len(list_2) != 0:
-        a = min(list_1[0],list_2[0])
-        if a == list_1[0]:
-            list_1.pop(0)
+    
+    while len(liste_1) != 0 and len(liste_2) != 0:
+        if liste_1[0] < liste_2[0]:
+            liste_r.append(liste_1[0])
+            liste_1.pop(0)
         else:
-            list_2.pop(0)
-        liste_r.append(a)
-    return list_r
+            liste_r.append(liste_2[0])
+            liste_2.pop(0)
+        
+    return liste_r + liste_1 + liste_2
+
+def fusion_listes_r(liste_1,liste_2):
+    if liste_2 == []:
+        return liste_1
+    elif liste_1 == []:
+        return liste_2
+    else:
+        if liste_1[0] < liste_2[0]:
+            return [liste_1[0]] + fusion_listes_r(liste_1[1:],liste_2)
+        else:
+            return [liste_2[0]] + fusion_listes_r(liste_2[1:],liste_1)
+
+def tri_fusion(liste):
+    if len(liste) == 1:
+        return liste
+    else:
+        mi = len(liste) // 2
+        return fusion_listes_r(tri_fusion(liste[:mi]), tri_fusion(liste[mi:]))
+    
 
     
 
 ## Déclaration des classes
 
+
 ## fonction principale
 def main():
-    pass
-
+    liste = [random.randint(0,1000) for i in range(10)]
+    
+    print(liste)
+    print(tri_fusion(liste))
 ## programme principale
 if __name__ == '__main__':
     main()
